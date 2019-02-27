f0984993abccbee01b4e0b698cdf0efd108ea163
Dennis Chung <chungdennis6@gmail.com>
2019-02-27 22:42:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+    winner_of_season = []
+  data[season].each do |season_hash|
+    if(season_hash["status"] == "Winner")
+       winner_of_season << season_hash["name"]
+    end
+  end
+  winner_of_season[0].split.first
 end
 
 def get_contestant_name(data, occupation)
